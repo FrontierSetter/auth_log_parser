083160f8cb6538f65066481e93f5b96add4ef342
Lu Yihui <lu_yihui@126.com>
2020-01-19 16:36:18 +0800

    parse auth.log & persist & insert ufw

--- parser.py
+++ parser.py
@@ -1,63 +1,105 @@
-import matplotlib.pyplot as plt
-from pyecharts.charts import Bar
-from pyecharts import options as opts
-file = open('./auth.log', 'r')
+import time
+import os
+import sys
 
-attackerDict = {}
+sudoPassword = sys.argv[1]
+
+# ========= get history ===========
+attackerDictOld = {}
+fileOld = open('totalData', 'r')
+oldTime = float(fileOld.readline())
 
 while True:
-    curLine = file.readline()
+    curLine = fileOld.readline().strip('\n')
     if curLine == '':
         break
     lineArr = curLine.split(' ')
-    if lineArr[5] == 'Failed':
-        curMonth = lineArr[0]
-        curDay = lineArr[1]
-        curTime = lineArr[2]
+    # print(lineArr)
+    attackerDictOld[lineArr[0]] = int(lineArr[1])
+
+fileOld.close()
+
+# ========================================
+
+
+
+# ================== parse auth.log ==============================
+attackerDict = {}
+validDict = {}
+curTime = 0.0
+
+fileRaw = open('./auth.log', 'r')
+
+while True:
+    curLine = fileRaw.readline()
+    if curLine == '':
+        break
+    
+    lineArr = curLine.split(' ')
+
+    # print(time.strptime(lineArr[0]+' '+lineArr[1]+' '+lineArr[2]+' 2020',"%b %d %H:%M:%S %Y"))
+    curTime = time.mktime(time.strptime(lineArr[0]+' '+lineArr[1]+' '+lineArr[2]+' 2020',"%b %d %H:%M:%S %Y"))
+    # print(time.mktime(time.strptime(lineArr[0]+' '+lineArr[1]+' '+lineArr[2],"%b %d %H:%M:%S")))
+
+    if curTime <= oldTime:
+        continue
+    
+    if 'Failed password' in curLine:
         fromIdx = lineArr.index('from')
-        targetUser = lineArr[fromIdx-1]
         attackerIP = lineArr[fromIdx+1]
-
-        # if targetUser == 'root':
-        #     attackerIP = lineArr[10]
-        # else:
-        #     targetUser = lineArr[10]
-        #     attackerIP = lineArr[12]
-            
-
         if attackerIP in attackerDict:
             attackerDict[attackerIP] += 1
         else:
             attackerDict[attackerIP] = 1
+    elif 'Accepted password' in curLine:
+        fromIdx = lineArr.index('from')
+        validIP = lineArr[fromIdx+1]
+        if validIP in validDict:
+            validDict[validIP] += 1
+        else:
+            validDict[validIP] = 1
 
-# attackerDictSorted = sorted(attackerDict,key=attackerDict.__getitem__,reverse=True)
+fileRaw.close()
+
+if curTime < oldTime:
+    curTime = oldTime
+# =================================================================================
+
+# ========================== exclude accepted ip ================================
+for k in validDict:
+    if k in attackerDict:
+        del attackerDict[k]
+# ===============================================================================
+
+# ===================== get new block target ======================
+newBlock = []
+
+for k in attackerDict:
+    curCnt = attackerDict[k]    # new fail cnt
+    if k in attackerDictOld:    # old fail cnt
+        curCnt += attackerDictOld[k]
+        if attackerDictOld[k] >= 10:    # should already blocked
+            print('error: '+k)
+    if (curCnt >= 10) and ((k not in attackerDictOld) or (attackerDictOld[k] < 10)):    # should block this time
+        newBlock.append(k)
+    attackerDictOld[k] = curCnt # update old
+
+# =============================================================================
+
+# ====================== block target ========================
+for i in newBlock:
+    print(i)
+    os.system('echo %s|sudo -S ufw insert 1 deny from %s' % (sudoPassword, i)) 
+    
+
+# ==============================================================================
 
 
-# plt.bar(range(len(attackerDict.values())), attackerDict.values(), color='rgb')
-# plt.show()
+fileTotal = open('totalData', 'w')
+fileTotal.write(str(curTime)+'\n')
+for k in attackerDictOld:
+    fileTotal.write(k+' '+str(attackerDictOld[k])+'\n')
 
-# bar = (
-#     Bar()
-#     .add_xaxis(["衬衫", "毛衣", "领带", "裤子", "风衣", "高跟鞋", "袜子"])
-#     .add_yaxis("商家A", [114, 55, 27, 101, 125, 27, 105])
-#     .add_yaxis("商家B", [57, 134, 137, 129, 145, 60, 49])
-#     .set_global_opts(title_opts=opts.TitleOpts(title="某商场销售情况"))
-# )
-# bar.render()
-
-attacker = []
-attacknum = []
-
-for k in sorted(attackerDict,key=attackerDict.__getitem__,reverse=True):
-    attacker.append(k)
-    attacknum.append(attackerDict[k])
-
-print('ss')
-
-bar = (
-    Bar()
-    .add_xaxis(attacker)
-    .add_yaxis("攻击者", attacknum)
-    .set_global_opts(title_opts=opts.TitleOpts(title="攻击情况"))
-)
-bar.render()
+print(attackerDict)
+print(attackerDictOld)
+# print(validDict)
